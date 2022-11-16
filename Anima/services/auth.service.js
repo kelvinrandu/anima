2ab7a3f86c1caadd5dbd864375473b583fd72dd9
Blackscure <wekesabuyahi@gmail.com>
2022-11-16 10:58:19 +0300

    added store

---
+++ Anima/services/auth.service.js
@@ -0,0 +1,32 @@
+import axios from "axios";
+
+const API_URL = "https://api.acts-pathways-academy.org/api/v1/auth/";
+
+class AuthService {
+  login(username, password) {
+    return axios
+      .post(API_URL + "login", { username, password })
+      .then((response) => {
+        if (response.data.accessToken) {
+          localStorage.setItem("user", JSON.stringify(response.data));
+        }
+        console.log('loggedin')
+
+        return response.data;
+      });
+  }
+
+  logout() {
+    localStorage.removeItem("user");
+  }
+
+  register(username, email, password) {
+    return axios.post(API_URL + "register", {
+      username,
+      email,
+      password,
+    });
+  }
+}
+
+export default new AuthService();
